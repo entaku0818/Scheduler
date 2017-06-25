ebf25506dde749a4ca4eb790db3ac6a789202f9e
遠藤拓弥 <endo@MacBook-Pro-4.local>
2017-06-25 23:58:12 +0900

    日付の指定ができるように変更する

--- ScheduleAdd/routes/facility.js
+++ ScheduleAdd/routes/facility.js
@@ -11,11 +11,14 @@ router.use(function(req, res, next) {
 	next();
 });
 /* GET home page. */
-router.get('/:facilityId', function(req, res, next) {
+router.get('/:facilityId/:date', function(req, res, next) {
+
+
 
 	facility_schedule.findAll({
 		where: {
-			facilityId: req.params.facilityId
+			facilityId: req.params.facilityId,
+			start: {gte: req.params.date,lte: moment(req.params.date).add("days", 1).format("YYYY-MM-DD")}
 		},
 		raw: true
 	}).then(function(result) {
@@ -33,64 +36,71 @@ router.post('/', function(req, res, next) {
 		var end = moment(data['end']).format();
 
 
-		facility_schedule.findAll({
-		  where: {
-				$or: [
-			    {start: {gte: start,lt: end}},
-					{end: {gt: start,lte: end}}
-				]
-			},
-			raw: true
-		}).then(function(result) {
-			console.log(result.length);
-			if(result.length > 0){
-				res.contentType('application/json');
-				res.send(JSON.stringify({status:1, test:"すでに予約が入っています(泣)"}));
-			}else{
-				facility_schedule.create(data)
-					.error(function(err) {
-						//エラー時の処理
-						console.log(err);
-						res.send('よやくしっぱい！！');
-					})
-					.then(function(result) {
-						//成功時の処理
+
+		    fs.writeFile('./add.json', JSON.stringify(data, null, '    '));
+
+				var exec = require('child_process').exec;
+
+
+				exec('casperjs ./facility_add.js', function(err, stdout, stderr){
+				  if (err) {
+				    console.error(err);
+				    res.status(500).send('エラー');
+				    return;
+				  }
+				  console.log(stdout);
+					if (stdout.indexOf('OK') != -1) {
+
+					}else{
+					}
+
+				  if (stderr) {
+				    console.error(stderr);
+				  }
+
+				});
+
+
+				facility_schedule.findAll({
+					where: {
+						$or: [
+							{start: {gte: start,lt: end}},
+							{end: {gt: start,lte: end}}
+						]
+					},
+					raw: true
+				}).then(function(result) {
+					console.log(result.length);
+					if(result.length > 0){
 						res.contentType('application/json');
-						res.send(JSON.stringify({status:0, test:"予約完了しました！！！"}));
-					});
-			}
-		});
+						res.send(JSON.stringify({status:1, test:"すでに予約が入っています(泣)"}));
+					}else{
+						facility_schedule.create(data)
+							.error(function(err) {
+								//エラー時の処理
+								console.log(err);
+								res.send('よやくしっぱい！！');
+							})
+							.then(function(result) {
+								//成功時の処理
+								res.contentType('application/json');
+								res.send(JSON.stringify({status:0, test:"予約完了しました！！！"}));
+							});
+					}
+				});
 
 
 
 
 
-
-    fs.writeFile('./add.json', JSON.stringify(data, null, '    '));
-
-		var exec = require('child_process').exec;
-
-		/*
-		exec('casperjs ./facility_add.js', function(err, stdout, stderr){
-		  if (err) {
-		    console.error(err);
-		    res.status(500).send('エラー');
-		    return;
-		  }
-		  console.log(stdout);
-			if (stdout.indexOf('OK') != -1) {
-				res.send('予約完了しました！！！');
-			}else{
-				res.send('よやくしっぱい！！');
-			}
-
-		  if (stderr) {
-		    console.error(stderr);
-		  }
-
-		});
-					*/
 })
+
+router.delete('/:facilityId', function(req, res, next) {
+
+
+			res.contentType('application/json');
+			res.send(JSON.stringify({status:0, test:"削除します(仮)"}));
+});
 
 
 module.exports = router;
